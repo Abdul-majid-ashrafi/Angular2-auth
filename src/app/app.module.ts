365c19d9377e082a7355d9b2baee31ccc050c352
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-01-24 02:32:36 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Router, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { SignupComponentComponent } from './signup-component/signup-component.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponentComponent },
+  { path: 'signup', component: SignupComponentComponent },
+  { path: '**', component: LoginComponentComponent } // for redirect
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent,
+    SignupComponentComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     FormsModule,
     HttpModule
@@ -17,4 +29,7 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private router: Router) {
+  }
+}

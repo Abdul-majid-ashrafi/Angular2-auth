f064e847b3d70af691596f5b09f9ccde1fa799db
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-01-24 07:04:08 +0500

    Auth complete

--- src/app/login-component/login-component.component.ts
+++ src/app/login-component/login-component.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods } from 'angularfire2';
 
 @Component({
-  selector: 'app-login-component',
-  templateUrl: './login-component.component.html',
-  styleUrls: ['./login-component.component.css']
+    selector: 'app-login-component',
+    templateUrl: './login-component.component.html',
+    styleUrls: ['./login-component.component.css']
 })
 export class LoginComponentComponent implements OnInit {
+    data: any = {
+        email: ""
+    }
+    constructor(public af: AngularFire) {
+        this.af.auth.subscribe(auth => console.log("Login", auth));
+    }
 
-  constructor() { }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
+    loginFacebook() {
+        this.af.auth.login({
+            provider: AuthProviders.Facebook,
+            method: AuthMethods.Popup
+        });
+    }
+    loginGoogle() {
+        this.af.auth.login({
+            provider: AuthProviders.Google,
+            method: AuthMethods.Popup
+        });
+    }
+    login() {
+        this.af.auth.login({
+            email: this.data.email,
+            password: this.data.pass
+        },
+            {
+                provider: AuthProviders.Password,
+                method: AuthMethods.Password
+            });
+    }
 
 }
--- src/app/signup-component/signup-component.component.ts
+++ src/app/signup-component/signup-component.component.ts
@@ -1,36 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFire, FirebaseListObservable} from 'angularfire2';
-// import { firebase} from 'firebase';
+import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods } from 'angularfire2';
 
 
 @Component({
-  selector: 'app-signup-component',
-  templateUrl: './signup-component.component.html',
-  styleUrls: ['./signup-component.component.css']
+    selector: 'app-signup-component',
+    templateUrl: './signup-component.component.html',
+    styleUrls: ['./signup-component.component.css']
 })
 export class SignupComponentComponent implements OnInit {
-  items: FirebaseListObservable<any[]>;
-  data: any = {
-    email: ""
-  }
-  constructor(public af: AngularFire) {
-    this.items = af.database.list('/items');
-  }
+    items: FirebaseListObservable<any[]>;
+    data: any = {
+        email: ""
+    }
+    constructor(public af: AngularFire) {
+        // this.items = af.database.list('/users');
+        // this.items.subscribe(item => {console.log(item)})
+        // this.af.auth.subscribe(auth => console.log("auth",auth));
 
-  ngOnInit() {
-  }
+    }
 
+    ngOnInit() {
+    }
 
-  signUp() {
-    // console.log(this.data.email)
-    // console.log(this.data.pass)
-    this.af.auth().createUserWithEmailAndPassword(this.data.email, this.data.pass)
-      .catch((error: any) => {
-        console.log(error);
-      })
-      .then((users: any) => {
-        console.log(users);
-        firebase.database().ref("user/" + users.uid).set(this.data);
-      });
-  }
+    // logOut() {
+    //     this.af.auth.logout();
+    // }
+
+    signUp() {
+        this.af.auth.createUser({ email: this.data.email, password: this.data.pass })
+            .catch((error: any) => {
+                console.log(error);
+            })
+            .then((users: any) => {
+                this.af.database.object('/users/' + users.uid).set(this.data);
+                console.log(users);
+            });
+    }
 }
